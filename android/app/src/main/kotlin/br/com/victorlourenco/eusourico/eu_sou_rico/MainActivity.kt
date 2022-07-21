0c29657868275d68641ec79a88045dcf9e1e2875
victorlourenco-vhl <victorhlo12@hotmail.com>
2022-07-21 12:59:34 -0300

    first-commit

---
+++ android/app/src/main/kotlin/br/com/victorlourenco/eusourico/eu_sou_rico/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.victorlourenco.eusourico.eu_sou_rico
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text('Eu Sou Rico'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+
+        backgroundColor: Colors.blueGrey[50],
+
+        body: Center(
+          child: Image(
+            image: AssetImage('images/rubi.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
